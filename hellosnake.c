fca8c9191e84f1b1cbde9c0a3e49cba69c336fcb
Johannes Degler <johannes@degler-online.de>
2019-06-10 09:49:00 +0200

    Uses much better drawing implementation

--- hellosnake.c
+++ hellosnake.c
@@ -2,7 +2,6 @@
 #include <gb/gb.h>
 #include <gb/hardware.h>
 #include <gb/drawing.h>
-#include <stdio.h>
 
 #define STOPPED 0
 #define UP 1
@@ -26,8 +25,8 @@ UINT8 getPlayerCoordsIndex(UINT8 offset);
 
 UINT8 i, j, k; // Re-usable variables
 
-UINT8 playerLength = 14;
-UINT8 playerCoords[MAX_PLAYER_LENGTH][2];
+UINT8 playerLength = 40;
+UINT8 playerCoords[MAX_PLAYER_LENGTH + 1][2];
 UINT8 playerCoordsIndex = 0;
 
 UINT8 xToFieldCoordsCache[FIELD_WIDTH];
@@ -57,7 +56,9 @@ const unsigned char bkgData[] =
   0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
 };
 
-UINT8 backgroundTiles[17][20] = {{0}};
+UINT8 singleTile[1] = {0};
+const UINT8 borderRowTiles[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const UINT8 normalRowTiles[20] = {0, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 0};
 
 
 // Sprite tiles
@@ -86,7 +87,6 @@ void main() {
         HIDE_WIN;
         SHOW_SPRITES;
         SHOW_BKG;
-        //printf("%u,%u! ", playerCoords[playerCoordsIndex][0], playerCoords[playerCoordsIndex][1]);
 
         wait_vbl_done();
     }
@@ -104,9 +104,11 @@ void initGame() {
     set_bkg_data(0, 5, bkgData); // Store background data in BGK VRAM
 
     // Draw initial background
-    for(i = 0; i != 18; i++) {
-        set_bkg_tiles(0, i, 20, 1, backgroundTiles[0]);
+    set_bkg_tiles(0, 0, 20, 1, borderRowTiles);
+    for(i = 1; i != FIELD_HEIGHT + 1; i++) {
+        set_bkg_tiles(0, i, 20, 1, normalRowTiles);
     }
+    set_bkg_tiles(0, FIELD_HEIGHT + 1, 20, 1, borderRowTiles);
 
     // Start player at center coords
     for(i = 0; i != playerLength; i++) {
@@ -146,7 +148,7 @@ void movePlayer() {
     // Move coords array index to next position
     playerCoordsIndex--;
     if(playerCoordsIndex == -1) {
-        playerCoordsIndex = MAX_PLAYER_LENGTH - 1;
+        playerCoordsIndex = MAX_PLAYER_LENGTH;
     }
 
     // Move head
@@ -174,34 +176,26 @@ void movePlayer() {
 }
 
 void drawPlayer() {
-    for(i = 1; i != FIELD_HEIGHT + 1; i++) {
-        // Reset background tile row
-        for(j = 1; j != FIELD_WIDTH + 1; j++) {
-            backgroundTiles[i][j] = 4;
+    // Clear last tails posotion
+    singleTile[0] = 4;
+    k = getPlayerCoordsIndex(playerLength);
+    if(playerCoords[k][0] != 0) {
+        set_bkg_tiles(playerCoords[k][0], playerCoords[k][1], 1, 1, singleTile);
+    }
+
+    // Draw player tiles
+    for(j = playerLength - 1; j != -1; j--) {
+        if(j == 0) {
+            singleTile[0] = 1;
         }
-
-        // Add player tiles to background row
-        for(j = playerLength - 1; j != -1; j--) {
-            k = getPlayerCoordsIndex(j);
-            if(playerCoords[k][1] == i - 1) {
-                if(j == 0) {
-                    backgroundTiles[i][playerCoords[k][0]] = 1;
-                }
-                else if(j & 1) {
-                    backgroundTiles[i][playerCoords[k][0]] = 2;
-                }
-                else {
-                    backgroundTiles[i][playerCoords[k][0]] = 3;
-                }
-           }
+        else if(j & 1) {
+            singleTile[0] = 2;
         }
-
-        // Re-draw border (in case it was overwritten)
-         backgroundTiles[i][0] = 0;
-         backgroundTiles[i][19] = 0;
-
-
-       set_bkg_tiles(0, i, 20, 1, backgroundTiles[i]);
+        else {
+            singleTile[0] = 3;
+        }
+        k = getPlayerCoordsIndex(j);
+        set_bkg_tiles(playerCoords[k][0], playerCoords[k][1], 1, 1, singleTile);
     }
 }
 
@@ -214,11 +208,11 @@ UINT8 mapPlayerYToFieldCoords(UINT8 playerY) {
 }
 
 UINT8 getPlayerCoordsIndex(UINT8 offset) {
-    if(playerCoordsIndex + offset >= MAX_PLAYER_LENGTH) {
-        return playerCoordsIndex + offset - MAX_PLAYER_LENGTH;
+    if(playerCoordsIndex + offset > MAX_PLAYER_LENGTH) {
+        return playerCoordsIndex + offset - MAX_PLAYER_LENGTH - 1;
     }
     else if(playerCoordsIndex + offset < 0) {
-        return playerCoordsIndex + offset + MAX_PLAYER_LENGTH;
+        return playerCoordsIndex + offset + MAX_PLAYER_LENGTH + 1;
     }
     else {
         return playerCoordsIndex + offset;
